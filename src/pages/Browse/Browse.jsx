4f6b020676bc6eec0742b7d9f909d0f58501002a
Goblin <jackmord445@gmail.com>
2022-02-16 17:10:24 +0800

    New Page -> Browse

---
+++ src/pages/Browse/Browse.jsx
@@ -0,0 +1,18 @@
+import SearchBar from "../../uicomponents/SearchBar/SearchBar";
+
+function Browse(props){
+    return (
+        <div>
+            <form>
+                <SearchBar />
+            </form>
+
+            <div className="flex-wrap flex flex-col justify-center items-center gap-3 mt-5">
+                
+            </div>
+
+        </div>
+    )
+}
+
+export default Browse;
