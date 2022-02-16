a9667e929d100b019f7d11e20264a3506f4b3a79
Goblin <jackmord445@gmail.com>
2022-02-16 17:16:37 +0800

    Search bar ui component

---
+++ src/uicomponents/SearchBar/SearchBar.js
@@ -0,0 +1,31 @@
+import style from './SearchBar.module.css';
+
+function SearchBar(props) {
+
+  return (
+    <div className={`${style.searchBarContainer} text-center mt-3`}>
+      <span>
+        <svg
+          className="feather feather-search"
+          height="24"
+          width="24"
+          fill="none"
+          stroke="#000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle cx="11" cy="11" r="8" />
+          <line x1="21" x2="16.65" y1="21" y2="16.65" />
+        </svg>
+      </span>
+
+      <input type="text" placeholder="Search" className={style.searchBar} />
+    </div>
+  );
+  
+}
+
+export default SearchBar;
