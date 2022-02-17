7ec1a29d35311b675cb23fad1589fc34256b4392
goblin <jackmord445@gmail.com>
2022-02-17 19:38:21 +0800

    constants

---
+++ src/constants/pages.jsx
@@ -0,0 +1,9 @@
+import Browse from "../pages/Browse/Browse";
+import Library from "../pages/Library/Library";
+
+export const PAGES = {
+    library: Library,
+    browse: Browse
+}
+
+export const defaultPage = "browse";
