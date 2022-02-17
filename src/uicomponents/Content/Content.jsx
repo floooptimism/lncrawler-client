ed76afe36fd8c937b5e3e286235d7d175d714134
Goblin <jackmord445@gmail.com>
2022-02-17 19:41:13 +0800

    Create Content.jsx

---
+++ src/uicomponents/Content/Content.jsx
@@ -0,0 +1,10 @@
+
+function Content(props){
+    return (
+        <div className="md:pt-0 md:ml-56 transition-all ease-in duration-150">
+            {props.children}
+        </div>
+    )
+}
+
+export default Content;
