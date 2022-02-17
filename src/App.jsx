4cfd69f28f14b80bd5bb9b9365e32ef09f82aeb9
Goblin <jackmord445@gmail.com>
2022-02-17 19:42:38 +0800

    nothing really

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import './App.css';
 import Navbar from './uicomponents/Navbar/Navbar';
 import { useEffect } from 'react';
 import ViewStateRenderer from './uicomponents/ViewState/ViewStateRenderer';
+import Liaison from './functionalcomponents/Liaison/Liaison';
 
 
 function App() {
   useEffect(() => {
- 
+    window.liaison = Liaison;
 
   }, [])
   
