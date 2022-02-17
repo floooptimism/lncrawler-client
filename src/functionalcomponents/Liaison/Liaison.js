cf566562ed4dd05c5e761ef8dbdff287913e4024
Goblin <jackmord445@gmail.com>
2022-02-17 19:42:03 +0800

    Update Liaison.js

--- src/functionalcomponents/Liaison/Liaison.js
+++ src/functionalcomponents/Liaison/Liaison.js
@@ -1,87 +1,103 @@
-const axios = require('axios');
+const axios = require("axios");
 
-class Liaison{
-    constructor(){
-        this.backendURL = 'https://lncrawler.azurewebsites.net';
+class Liaison {
+  constructor() {
+    this.backendURL = "https://lncrawler.azurewebsites.net";
+  }
+
+  //TODO: ERROR HANDLING FOR EVERYTHING!!!!!
+
+  async getNovelInfo(novelURL, novelSource) {
+    const postfix = "/novelinfo";
+    const param_scraperSource = "scraperSource=" + novelSource;
+    const param_novelURL = "novelURL=" + novelURL;
+    const url =
+      this.backendURL +
+      postfix +
+      "?" +
+      param_scraperSource +
+      "&" +
+      param_novelURL;
+
+    try {
+      let res = await axios.get(url);
+      console.log(res);
+      return res.data;
+    } catch (err) {
+      return null;
     }
+  }
 
-    //TODO: ERROR HANDLING FOR EVERYTHING!!!!!
+  async getNovelChapters(novelURL, novelSource) {
+    const postfix = "/chapters";
+    const param_scraperSource = "scraperSource=" + novelSource;
+    const param_novelURL = "novelURL=" + novelURL;
+    const url =
+      this.backendURL +
+      postfix +
+      "?" +
+      param_scraperSource +
+      "&" +
+      param_novelURL;
 
-    async getNovelInfo(novelURL, novelSource){
-        const postfix = "/novelinfo";
-        const param_scraperSource = "scraperSource=" + novelSource;
-        const param_novelURL = "novelURL=" + novelURL;
-        const url = this.backendURL + postfix + "?" + param_scraperSource + "&" + param_novelURL;
-
-        try{
-            let res = await axios.get(url);
-            console.log(res);
-            return res.data;
-        }catch(err){
-            return null;
-        }
-
+    try {
+      let res = await axios.get(url);
+      console.log("getNovelChapters result!!!!");
+      console.log(res);
+      return res.data;
+    } catch (err) {
+      // handle error
+      console.log("getNovelChapters error!!!!");
+      console.log(err);
+      return null;
     }
+  }
 
+  async searchNovel(novelName, novelSource) {
+    const postfix = "/search";
+    const param_scraperSource = "scraperSource=" + novelSource;
+    const param_novelName = "novelName=" + novelName;
+    const url =
+      this.backendURL +
+      postfix +
+      "?" +
+      param_scraperSource +
+      "&" +
+      param_novelName;
 
-    async getNovelChapters(novelURL, novelSource){
-        const postfix = "/chapters";
-        const param_scraperSource = "scraperSource=" + novelSource;
-        const param_novelURL = "novelURL=" + novelURL;
-        const url = this.backendURL + postfix + "?" + param_scraperSource + "&" + param_novelURL;
-
-        try{
-            let res = await axios.get(url);
-            console.log("getNovelChapters result!!!!");
-            console.log(res);
-            return res.data;
-        }catch(err){
-            // handle error
-            console.log("getNovelChapters error!!!!");
-            console.log(err);
-            return null;
-        }
-    
+    try {
+      let res = await axios.get(url);
+      console.log("Search novel result!!!!");
+      console.log(res);
+      return res.data;
+    } catch (err) {
+      console.log("Search novel error!!!!");
+      console.log(err);
+      return null;
     }
+  }
 
+  async getNovelChapterContent(chapterURL, novelSource) {
+    const postfix = "/chaptercontent";
+    const param_scraperSource = "scraperSource=" + novelSource;
+    const param_chapterURL = "chapterURL=" + chapterURL;
+    const url =
+      this.backendURL +
+      postfix +
+      "?" +
+      param_scraperSource +
+      "&" +
+      param_chapterURL;
 
-    async searchNovel(novelName, novelSource){
-        const postfix = "/search";
-        const param_scraperSource = "scraperSource=" + novelSource;
-        const param_novelName = "novelName=" + novelName;
-        const url = this.backendURL + postfix + "?" + param_scraperSource + "&" + param_novelName;
-
-        try{
-            let res = await axios.get(url);
-            console.log("Search novel result!!!!");
-            console.log(res);
-            return res.data;
-        }catch(err){
-            console.log("Search novel error!!!!");
-            console.log(err);
-            return null;
-        }
-
+    try {
+      let res = await axios.get(url);
+      console.log(res);
+      return res.data;
+    } catch (err) {
+      console.log(err);
+      return null;
     }
-
-    
-    async getNovelChapterContent(chapterURL, novelSource){
-        const postfix = "/chaptercontent";
-        const param_scraperSource = "scraperSource=" + novelSource;
-        const param_chapterURL = "chapterURL=" + chapterURL;
-        const url = this.backendURL + postfix + "?" + param_scraperSource + "&" + param_chapterURL;
-
-        try{
-            let res = await axios.get(url);
-            console.log(res);
-            return res.data;
-        }catch(err){
-            console.log(err);
-            return null;
-        }
-
-    }
-
+  }
 }
 
-export default Liaison;
+export default new Liaison();
