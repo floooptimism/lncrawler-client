eb1c3acdbe3ddde75afb4fb42e28757817a98830
goblin <jackmord445@gmail.com>
2022-02-19 15:43:11 +0800

    design for mobile novel card - page browse

--- src/uicomponents/BrowseNovelCard/BrowseNovelCard.jsx
+++ src/uicomponents/BrowseNovelCard/BrowseNovelCard.jsx
@@ -7,6 +7,34 @@ function BrowseNovelCard({title, author, description, cover, url, source}){
             <div className={`${styles.BrowseNovelCardImageContainer}`}>
                 <img src={cover} alt={title} className={`${styles.BrowseNovelCardImage}`} />
             </div>
+
+            <div className={styles.BrowseNovelCardInfo}>
+                {/* title */}
+                <div className={styles.BrowseNovelCardTitle}>
+                    <a href={url} target="_blank" rel="noopener noreferrer">
+                        {title}
+                    </a>
+                </div>
+
+                {/* author */}
+                <div className={styles.BrowseNovelCardAuthor}>
+                    {author}
+                </div>
+
+                {/* description */}
+                <div className={styles.BrowseNovelCardDescription}>
+                    {/* fade  */}
+                    <div className={styles.BrowseNovelCardDescriptionFade}>
+                    </div>       
+                    {description}
+                </div>
+
+                {/* Indicator if Novel is already in library or not */}
+                <div className={styles.BrowseNovelCardIndicator + " " + styles.BrowseNovelCardIndicatorInLibrary}>
+                    In Library
+                </div>
+
+            </div>
         </div>
     )
 }
