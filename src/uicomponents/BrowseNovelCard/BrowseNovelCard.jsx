6b90c13b10747e2c49e542b605a6ea2861607bae
goblin <jackmord445@gmail.com>
2022-02-21 01:01:52 +0800

    *hold

--- src/uicomponents/BrowseNovelCard/BrowseNovelCard.jsx
+++ src/uicomponents/BrowseNovelCard/BrowseNovelCard.jsx
@@ -1,72 +1,76 @@
 import styles from "./BrowseNovelCard.module.css";
 
 function BrowseNovelCard({
-    title,
-    author,
-    description,
-    cover,
-    url,
-    source,
-    inLibrary,
+  title,
+  author,
+  description,
+  cover,
+  url,
+  source,
+  onClick,
+  inLibrary,
 }) {
-
-    function toggleInLibrary() {
-        if (inLibrary) {
-            // remove from library
-            return;
-        }
-        
-        // add to library
+  function toggleInLibrary() {
+    if (inLibrary) {
+      // remove from library
+      return;
     }
 
-function BrowseNovelCard({title, author, description, cover, url, source, onClick}){
-    return (
-        <div onClick={onClick.bind(null, url, source)} className={`${styles.BrowseNovelCardContainer}`}>
-            {/* image */}
-            <div className={`${styles.BrowseNovelCardImageContainer}`}>
-                <img
-                    src={cover}
-                    alt={title}
-                    className={`${styles.BrowseNovelCardImage}`}
-                />
-            </div>
+    // add to library
+  }
 
-            <div className={styles.BrowseNovelCardInfo}>
-                {/* title */}
-                <div className={styles.BrowseNovelCardTitle}>
-                    <a href={url} target="_blank" rel="noopener noreferrer">
-                        {title}
-                    </a>
-                </div>
+  function clickHandle(){
+    onClick(url, source, inLibrary);
+  }
 
-                {/* author */}
-                <div className={styles.BrowseNovelCardAuthor}>{author}</div>
+  return (
+    <div
+      onClick={clickHandle}
+      className={`${styles.BrowseNovelCardContainer}`}
+    >
+      {/* image */}
+      <div className={`${styles.BrowseNovelCardImageContainer}`}>
+        <img
+          src={cover}
+          alt={title}
+          className={`${styles.BrowseNovelCardImage}`}
+        />
+      </div>
 
-                {/* description */}
-                <div className={styles.BrowseNovelCardDescription}>
-                    {/* fade  */}
-                    <div
-                        className={styles.BrowseNovelCardDescriptionFade}
-                    ></div>
-                    {description}
-                </div>
-
-                {/* Indicator if Novel is already in library or not */}
-                <div
-                    onClick={toggleInLibrary}
-                    className={
-                        styles.BrowseNovelCardIndicator +
-                        " " +
-                        (inLibrary
-                            ? styles.BrowseNovelCardIndicatorInLibrary
-                            : styles.BrowseNovelCardIndicatorNotInLibrary)
-                    }
-                >
-                    {inLibrary ? "In Library" : "Add to Library"}
-                </div>
-            </div>
+      <div className={styles.BrowseNovelCardInfo}>
+        {/* title */}
+        <div className={styles.BrowseNovelCardTitle}>
+          <a href={url} target="_blank" rel="noopener noreferrer">
+            {title}
+          </a>
         </div>
-    );
+
+        {/* author */}
+        <div className={styles.BrowseNovelCardAuthor}>{author}</div>
+
+        {/* description */}
+        <div className={styles.BrowseNovelCardDescription}>
+          {/* fade  */}
+          <div className={styles.BrowseNovelCardDescriptionFade}></div>
+          {description}
+        </div>
+
+        {/* Indicator if Novel is already in library or not */}
+        <div
+          onClick={toggleInLibrary}
+          className={
+            styles.BrowseNovelCardIndicator +
+            " " +
+            (inLibrary
+              ? styles.BrowseNovelCardIndicatorInLibrary
+              : styles.BrowseNovelCardIndicatorNotInLibrary)
+          }
+        >
+          {inLibrary ? "In Library" : "Add to Library"}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default BrowseNovelCard;
