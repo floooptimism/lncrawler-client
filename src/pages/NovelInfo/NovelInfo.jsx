08d89c38b7fdb385f02362adaff58e2a5338a478
goblin <jackmord445@gmail.com>
2022-02-21 15:44:37 +0800

    WIP NovelInfo Modal

--- src/pages/NovelInfo/NovelInfo.jsx
+++ src/pages/NovelInfo/NovelInfo.jsx
@@ -1,54 +1,134 @@
-import { useEffect } from 'react';
-import Liaison from '../../functionalcomponents/Liaison/Liaison';
-import styles from './NovelInfo.module.css';
+import { useEffect } from "react";
+import Liaison from "../../functionalcomponents/Liaison/Liaison";
+import styles from "./NovelInfo.module.css";
 
-
-function NovelInfo(props){
-
+function NovelInfo(props) {
     useEffect(() => {
-        async function getNovelInfo(){
+        async function getNovelInfo() {
             let res = await Liaison.getNovelInfo(props.url, props.scraper);
             console.log(res);
         }
-        
+
         getNovelInfo();
     }, [props.url, props.scraper]);
 
     return (
         <div className={styles.background}>
-
             <div className={styles.NovelInfo}>
-                {/* image cover */}
-                <div className={styles.Cover}>
-                    <img src={props.cover} alt="cover" />
+                {/* fixed topbar */}
+                <div className={styles.NovelInfoTopBar}>
+                    {/* go back icon */}
+                    <div className={styles.NovelInfoTopBarIcon}>
+                        <svg    
+                            onClick={props.onClose}
+                            height="24"
+                            width="24"
+                            viewBox="0 0 48 48"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path d="M42 22H13.66l7.17-7.17L18 12 6 24l12 12 2.83-2.83L13.66 26H42z" />
+                        </svg>
+                    </div>
+
+                    {/* title */}
+                    <div className={styles.NovelInfoTopBarTitle}>
+                        Novel Info
+                    </div>
+
+                    {/* ellipsis icon*/}
+                    <div className={styles.NovelInfoTopBarIcon}>
+                        <svg
+                            height="20"
+                            width="20"
+                            viewBox="0 0 512 512"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <title>ionicons-v5-f</title>
+                            <circle cx="256" cy="256" r="48" />
+                            <circle cx="256" cy="416" r="48" />
+                            <circle cx="256" cy="96" r="48" />
+                        </svg>
+                    </div>
                 </div>
 
-                {/* title */}
-                <div className={styles.Title}>
-                    <h1>{props.title}</h1>
-                </div>
+                <div className={styles.NovelInfoContent}>
+                    <h1>Dudeeee</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
+                    <h1>Dude</h1>
 
-                {/* author */}
-                <div className={styles.Author}>
-                    <h2>{props.author}</h2>
-                </div>
+                    {/* image cover */}
+                    <div className={styles.Cover}>
+                        <img src={props.cover} alt="cover" />
+                    </div>
 
-                {/* description */}
-                <div className={styles.Description}>
-                    <p>{props.description}</p>
-                </div>
+                    {/* author */}
+                    <div className={styles.Author}>
+                        <h2>{props.author}</h2>
+                    </div>
 
-                {/* information grid */}
-                <div className={styles.Information}>
-                    <div className={styles.InformationItem}>
-                        <h3>{props.chapters}</h3>
-                        <p>Chapters</p>
+                    {/* description */}
+                    <div className={styles.Description}>
+                        <p>{props.description}</p>
+                    </div>
+
+                    {/* information grid */}
+                    <div className={styles.Information}>
+                        <div className={styles.InformationItem}>
+                            <h3>{props.chapters}</h3>
+                            <p>Chapters</p>
+                        </div>
                     </div>
                 </div>
             </div>
-            
         </div>
-    )
+    );
 }
 
 export default NovelInfo;
