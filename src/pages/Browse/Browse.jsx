b4e83dea2dfaf0850f1363956abe0fe505fe260e
Goblin <goblin.wx7p6@aleeas.com>
2022-02-24 17:13:50 +0800

    change ui

--- src/pages/Browse/Browse.jsx
+++ src/pages/Browse/Browse.jsx
@@ -7,103 +7,101 @@ import BrowseNovelCard from "../../uicomponents/BrowseNovelCard/BrowseNovelCard"
 import useLibrarian from "../../hooks/useLibrarian";
 import NovelInfo from "../NovelInfo/NovelInfo";
 
-import scrollControlFunctions from './disableScrolling'
-const {enableScroll, disableScroll} = scrollControlFunctions;
+import scrollControlFunctions from "./disableScrolling";
+const { enableScroll, disableScroll } = scrollControlFunctions;
 
 function Browse(props) {
-  const [searchResults, setSearchResults] = useState([]);
-  const [searched, setSearched] = useState(false);
-  const [loading, setLoading] = useState(false);
+    const [searchResults, setSearchResults] = useState([]);
+    const [searched, setSearched] = useState(false);
+    const [loading, setLoading] = useState(false);
 
-  const {novels, novelIds, librarian} = useLibrarian();
+    const {novelIds, librarian } = useLibrarian();
 
-  async function searchNovel(novelName, source) {
-    setSearched(true);
-    setLoading(true);
-    let res = await Liaison.searchNovel(novelName, source);
-    console.log(res);
-    setLoading(false);
-    setSearchResults(res);
-  }
+    async function searchNovel(novelName, source) {
+        setSearched(true);
+        setLoading(true);
+        let res = await Liaison.searchNovel(novelName, source);
+        console.log(res);
+        setLoading(false);
+        setSearchResults(res);
+    }
 
-  return (
-    <div className={styles.BrowseContainer}>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          searchNovel(e.target[0].value, e.target[1].value);
-        }}
-      >
-        <SearchBar className="w-full"/>
+    window.callme = function(){
+      librarian.testing("https://lightnovel.world/book/1616/19.html")
+    }
 
-        {/* Dropdown select source */}
-        <div className={`${styles.SourceDropdownContainer}`}>
-          <label htmlFor="source">Select Source</label>
-          <select id="source" className={`${styles.SourceDropdown}`}>
-            <option value="A">All sources</option>
-            <option value="https://lightnovel.world">
-              www.lightnovel.world
-            </option>
-          </select>
+    useEffect( () =>{
+      console.log("Novelids", novelIds);
+    }, [novelIds])
+
+    return (
+        <div className={styles.BrowseContainer}>
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    searchNovel(e.target[0].value, e.target[1].value);
+                }}
+            >
+                <SearchBar className="w-full" />
+
+                {/* Dropdown select source */}
+                <div className={`${styles.SourceDropdownContainer}`}>
+                    <label htmlFor="source">Select Source</label>
+                    <select id="source" className={`${styles.SourceDropdown}`}>
+                        <option value="A">All sources</option>
+                        <option value="https://lightnovel.world">
+                            www.lightnovel.world
+                        </option>
+                    </select>
+                </div>
+            </form>
+
+            <div className="relative flex-wrap flex flex-col justify-center items-center gap-3 py-8">
+                {/* if searched and searchResults is empty, tell user that there are no results  */}
+                {searched && !loading && searchResults.length === 0 && (
+                    <p className={styles.Info}>No results found</p>
+                )}
+
+                {/* if loading, tell user that it's loading */}
+                {loading && <p className={styles.Info}>Loading...</p>}
+
+                {/* if not searched, tell user to enter a novel name and select source */}
+                {!searched && (
+                    <p className={styles.Info}>
+                        Enter a novel name and select a source
+                    </p>
+                )}
+
+                {/* if searchResults is not empty, display the results */}
+                {!loading && searchResults.length > 0 && (
+                    <DisplayResults
+                        arrayOfResults={searchResults}
+                        novelIds={novelIds}
+                        librarian={librarian}
+                    />
+                )}
+            </div>
         </div>
-      </form>
-
-      <div className="relative flex-wrap flex flex-col justify-center items-center gap-3 py-8">
-
-        {/* if searched and searchResults is empty, tell user that there are no results  */}
-        {searched && !loading && searchResults.length === 0 && (
-          <p className={styles.Info}>
-            No results found
-          </p>
-        )}
-
-        {/* if loading, tell user that it's loading */}
-        {loading && (
-          <p className={styles.Info}>
-            Loading...
-          </p>
-        )}
-
-        {/* if not searched, tell user to enter a novel name and select source */}
-        {!searched && (
-          <p className={styles.Info}>
-            Enter a novel name and select a source
-          </p>
-        )}
-
-        {/* if searchResults is not empty, display the results */}
-        {!loading &&
-          searchResults.length > 0 &&
-          <DisplayResults arrayOfResults={searchResults} novelIds={novelIds} librarian={librarian}/>
-        }
-
-
-      </div>
-    </div>
-  );
+    );
 }
 
-
 // partial loading
-function DisplayResults({arrayOfResults, novelIds, librarian}) {
+function DisplayResults({ arrayOfResults, novelIds, librarian }) {
     const [idx, setIdx] = useState();
     const [results, setResults] = useState([]);
     const [novelInfoModal, setNovelInfoModal] = useState(false);
     const [novelInfoParam, setNovelInfoParam] = useState({});
-    const [removeScroll, setRemoveScroll] = useState(false);
 
-    function openModal(url, source, inLibrary){
-      setNovelInfoModal(true);
-      setNovelInfoParam({url, source, inLibrary});
-
+    function openModal(url, source, inLibrary) {
+        setNovelInfoModal(true);
+        setNovelInfoParam({ url, source, inLibrary });
     }
 
-    function closeModal(){
-      setRemoveScroll(false);
-      setNovelInfoModal(false);
+    function closeModal() {
+        setNovelInfoModal(false);
     }
 
-    function loadMore(){
+    function loadMore() {
         setIdx(idx + 10);
     }
 
@@ -142,14 +140,25 @@ function DisplayResults({arrayOfResults, novelIds, librarian}) {
         <>
             {results.map((result, idx) => {
                 return (
-                    <BrowseNovelCard onClick={openModal} {...result} inLibrary={result.url in novelIds} librarian={librarian}/>
-                )
-            })
-            }
+                    <BrowseNovelCard
+                        onClick={openModal}
+                        {...result}
+                        inLibrary={result.url in novelIds}
+                        librarian={librarian}
+                    />
+                );
+            })}
 
-            {novelInfoModal && <NovelInfo onClose={closeModal}  {...results[idx]} {...novelInfoParam} librarian={librarian}/>}
+            {novelInfoModal && (
+                <NovelInfo
+                    onClose={closeModal}
+                    {...results[idx]}
+                    {...novelInfoParam}
+                    librarian={librarian}
+                />
+            )}
         </>
-    )
+    );
 }
 
 export default Browse;
