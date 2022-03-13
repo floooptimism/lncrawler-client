5e9bdf570294839c3f7c8baba03b77de7539a85a
Goblin <goblin.wx7p6@aleeas.com>
2022-03-13 20:28:44 +0800

    made lots of changes
    use react-window for displaying chapters
    can now load chapters and also download if there are no chapters

--- src/pages/NovelInfoLibrary/Tabs.jsx
+++ src/pages/NovelInfoLibrary/Tabs.jsx
@@ -1,6 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Liaison from "../../functionalcomponents/Liaison/Liaison";
 
 import styles from "./Tabs.module.css";
+
+import { FixedSizeList as List} from 'react-window';
+import AutoSizer from "react-virtualized-auto-sizer";
 
 function Tabs(props) {
     const [active, setActive] = useState(0);
@@ -15,7 +19,7 @@ function Tabs(props) {
     }
 
     return (
-        <div>
+        <div className="h-full">
             <div>
                 <ul className="flex justify-evenly border-b border-gray-300">
                     {tabs.map((tab, index) => (
@@ -37,12 +41,16 @@ function Tabs(props) {
                 </ul>
             </div>
 
-            <div className="px-4 pt-5">
-                <RenderActiveTab />
+            <div className="px-4 pt-5 h-full">
+                <RenderActiveTab {...props}/>
             </div>
         </div>
     );
 }
+
+
+
+
 
 function InformationTab(props) {
     return (
@@ -72,16 +80,81 @@ function InformationTab(props) {
     );
 }
 
-function ChaptersTab({chapters=[]}) {
+function ChaptersTab({librarian, novelInfo}) {
+    const [chapters, setChapters] = useState([]);
+
+    async function downloadChapters(){
+        let chapters = await Liaison.getNovelChapters(novelInfo.url, novelInfo.source);
+        if(chapters.length > 0){
+            await librarian.storeChapters(novelInfo.url, chapters);
+            setChapters(chapters);
+        }
+    }
+
+    window.liaison = Liaison;
+    window.librarian = librarian;
+    
+    useEffect(()=>{
+        librarian.getChapters(novelInfo.url).then(res => {
+            setChapters(res);
+        })
+    }, [librarian, novelInfo.url]);
+
+    const Item = function ({index, style}){
+        return (
+            <div key={index} className="flex py-2" style={style}>
+                <div>
+                    <h1 className="text-sm font-medium items-center">Chapter {index + 1}</h1>
+                    <p className="text-xs">{chapters[index].name}</p>
+                </div>
+                
+                <div className="ml-auto flex items-center">
+                    <svg width="20" height="20" viewBox="0 0 18 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M8 0V16.5625L2.71875 11.2812L1.28125 12.7188L8.28125 19.7188L9 20.4062L9.71875 19.7188L16.7188 12.7188L15.2812 11.2812L10 16.5625V0H8ZM0 22V24H18V22H0Z" fill="#6D6C6C"/>
+                    </svg>
+    
+                </div>
+    
+            </div>
+        )
+    }
+
+
     return (
         <>
-            <div className="px-4" style={{color: "#565656"}}>
-                <ul>
-                    {chapters.length > 0 && chapters.map((chapter, index) => (
+            <div className="px-4" style={{color: "#565656",height:"100%"}}>
+                {chapters.length === 0 && (
+                    <div>
+                        <h1 className="text-center">No chapters in library</h1>
+                        <p className="text-center">
+                            Add chapters to your library by clicking the button
+                            below
+                            </p>
+                            <button onClick={downloadChapters}>Load chapters</button>
+                    </div>
+                )}
+
+                {chapters.length > 0 && (
+                    <AutoSizer>
+                        {({ height, width }) => (
+                            <List
+                                height={height}
+                                itemCount={chapters.length}
+                                itemSize={35}
+                                width={width}
+                            >
+                                {Item}
+
+                            </List>
+                        )}
+                    </AutoSizer>
+                )}
+                {/* <ul>
+                    {chapters.length > 0 && chapters.slice(0,500).map((chapter, index) => (
                         <li key={index} className="flex py-2">
                             <div>
                                 <h1 className="text-sm font-medium items-center">Chapter {index + 1}</h1>
-                                <p className="text-xs">{chapter.title}</p>
+                                <p className="text-xs">{chapter.name}</p>
                             </div>
                             
                             <div className="ml-auto flex items-center">
@@ -93,10 +166,13 @@ function ChaptersTab({chapters=[]}) {
 
                         </li>
                     ))}
-                </ul>
+                </ul> */}
             </div>
         </>
     );
 }
+
+
+
 
 export default Tabs;
