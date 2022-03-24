10ea796a7abfc3bf21bc1c8e06b51a24e2fa8cfb
goblin <jackmord445@gmail.com>
2022-03-24 11:17:48 +0800

    reader context

---
+++ src/contexts/ReaderContext/ReaderProvider.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect, useContext, createContext } from "react";
+
+const ReaderContext = createContext();
+
+function ReaderProvider(props) {
+    const [readerIsOpen, setReaderIsOpen] = useState(false);
+    const [readerContent, setReaderContent] = useState("");
+    const [readerNovelInfo, setReaderNovelInfo] = useState(null);
+    const [readerChapterInfo, setReaderChapterInfo] = useState(null);
+
+    return (
+        <ReaderContext.Provider
+            value={{
+                readerIsOpen,
+                setReaderIsOpen,
+                readerContent,
+                setReaderContent,
+                readerNovelInfo,
+                setReaderNovelInfo,
+                readerChapterInfo,
+                setReaderChapterInfo,
+            }}
+        >
+            {props.children}
+        </ReaderContext.Provider>
+    );
+}
+
+
+export default ReaderProvider;
+
+
+function useReader() {
+    const context = useContext(ReaderContext);
+    if (!context) {
+        throw new Error("useReader must be used within a ReaderProvider");
+    }
+    return context;
+}
+
+export  {useReader};
