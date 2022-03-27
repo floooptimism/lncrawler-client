d5ad179b81df6cb05159417ca514dcdb5d91e61a
goblin <jackmord445@gmail.com>
2022-03-27 18:59:11 +0800

    use memoization and fix scroll reset

--- src/pages/NovelInfoLibrary/Tabs/ChaptersTab.jsx
+++ src/pages/NovelInfoLibrary/Tabs/ChaptersTab.jsx
@@ -26,7 +26,7 @@ function ChaptersTab({ librarian, novelInfo }) {
     const [initDone, setInitDone] = useState(false);
     const [downloadedChapters, setDownloadedChapters] = useState([]);
     const { menuComponentId, setMenuComponentId, setMenuComponent } =
-    useNovelInfoLibraryContext();
+        useNovelInfoLibraryContext();
 
     function Component(props) {
         return (
@@ -47,7 +47,6 @@ function ChaptersTab({ librarian, novelInfo }) {
         setMenuComponentId(ComponentId);
         setMenuComponent(Component);
     }, [menuComponentId, setMenuComponent, setMenuComponentId]);
-
 
     async function fetchChaptersInfo() {
         let chapters = await librarian.getChapters(novelInfo.url);
@@ -95,86 +94,6 @@ function ChaptersTab({ librarian, novelInfo }) {
 
     // set the downloads menu
 
-    const Item = function ({ index, style }) {
-       
-        const { functions: Downloader } = useDownloader();
-
-        const {setReaderIsOpen} = useReader();
-
-        function handleClick(){
-            setReaderIsOpen(true);
-        }
-
-        function DownloadAllChapters() {
-            let tasksToDownload = chapters.map((chapter) => {
-                return chapterToTask(novelInfo, chapter);
-            });
-            Downloader.bulkAddTask(tasksToDownload);
-        }
-
-        function CancelAllChapters() {}
-
-        
-        function download(e) {
-            e.stopPropagation();
-            let chapter = chapters[index];
-            Downloader.addTask(chapterToTask(novelInfo, chapter, index + 1));
-
-        }
-
-        return (
-            <div key={index} className="flex py-2 px-5 cursor-pointer" style={style} onClick={handleClick}>
-                <div>
-                    <h1 className="text-sm font-medium items-center">
-                        Chapter {index + 1}
-                    </h1>
-                    <p className="text-xs">{chapters[index].name}</p>
-                </div>
-
-                <div className="ml-auto flex items-center">
-                    {(downloadedChapters[index] && (
-                        <svg
-                            style={{
-                                color: "#65C18C"
-                            }}
-                            className="fill-current"
-                            height="20"
-                            width="20"
-                            viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z" />
-                        </svg>
-                    )) ||
-                        (Downloader.inQueue &&
-                            Downloader.inQueue([
-                                novelInfo.url + chapters[index].url,
-                            ]) && (
-                                <MoonLoader
-                                    color="blue"
-                                    loading={true}
-                                    size={18}
-                                />
-                            )) || (
-                            <svg
-                                onClick={download}
-                                width="20"
-                                height="20"
-                                viewBox="0 0 18 24"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    d="M8 0V16.5625L2.71875 11.2812L1.28125 12.7188L8.28125 19.7188L9 20.4062L9.71875 19.7188L16.7188 12.7188L15.2812 11.2812L10 16.5625V0H8ZM0 22V24H18V22H0Z"
-                                    fill="#6D6C6C"
-                                />
-                            </svg>
-                        )}
-                </div>
-            </div>
-        );
-    };
-
     return (
         <>
             <div className="px-1" style={{ color: "#565656", height: "100%" }}>
@@ -197,21 +116,7 @@ function ChaptersTab({ librarian, novelInfo }) {
                     </div>
                 )}
 
-                {chapters.length > 0 && (
-                    <AutoSizer>
-                        {({ height, width }) => (
-                            <List
-                                height={height}
-                                itemCount={chapters.length}
-                                itemSize={52}
-                                width={width}
-                                className="test"
-                            >
-                                {Item}
-                            </List>
-                        )}
-                    </AutoSizer>
-                )}
+                <MemoizedMyList chapters={chapters} novelInfo={novelInfo} downloadedChapters={downloadedChapters} />
                 {/* <ul>
                     {chapters.length > 0 && chapters.slice(0,500).map((chapter, index) => (
                         <li key={index} className="flex py-2">
@@ -234,5 +139,108 @@ function ChaptersTab({ librarian, novelInfo }) {
         </>
     );
 }
+
+
+
+
+const Item = function ({data, index, style}) {
+    const { functions: Downloader } = useDownloader();
+    
+    const { chapters, novelInfo, downloadedChapters, setReaderIsOpen } = data;
+    
+    function handleClick() {
+        setReaderIsOpen(true);
+    }
+
+    function download(e) {
+        e.stopPropagation();
+        let chapter = chapters[index];
+        Downloader.addTask(chapterToTask(novelInfo, chapter, index + 1));
+    }
+
+    if(chapters[index] === undefined) { return null; }
+
+    return (
+        <div
+            key={index}
+            className="flex py-2 px-5 cursor-pointer"
+            style={style}
+            onClick={handleClick}
+        >
+            <div>
+                <h1 className="text-sm font-medium items-center">
+                    Chapter {index + 1}
+                </h1>
+                <p className="text-xs">{chapters[index].name}</p>
+            </div>
+
+            <div className="ml-auto flex items-center">
+                {(downloadedChapters[index] && (
+                    <svg
+                        style={{
+                            color: "#65C18C",
+                        }}
+                        className="fill-current"
+                        height="20"
+                        width="20"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z" />
+                    </svg>
+                )) ||
+                    (Downloader.inQueue &&
+                        Downloader.inQueue([
+                            novelInfo.url + chapters[index].url,
+                        ]) && (
+                            <MoonLoader
+                                color="blue"
+                                loading={true}
+                                size={18}
+                            />
+                        )) || (
+                        <svg
+                            onClick={download}
+                            width="20"
+                            height="20"
+                            viewBox="0 0 18 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M8 0V16.5625L2.71875 11.2812L1.28125 12.7188L8.28125 19.7188L9 20.4062L9.71875 19.7188L16.7188 12.7188L15.2812 11.2812L10 16.5625V0H8ZM0 22V24H18V22H0Z"
+                                fill="#6D6C6C"
+                            />
+                        </svg>
+                    )}
+            </div>
+        </div>
+    );
+};
+
+function MyList({chapters, novelInfo, downloadedChapters}) {
+    const { setReaderIsOpen } = useReader();
+    console.log("RERENDERING MYLIST");
+
+    return (
+        <AutoSizer>
+            {({ height, width }) => (
+                <List
+                    itemData={{chapters, novelInfo, downloadedChapters, setReaderIsOpen}}
+                    height={height}
+                    itemCount={chapters.length}
+                    itemSize={52}
+                    width={width}
+                    className="test"
+                >
+                    {Item}
+                </List>
+            )}
+        </AutoSizer>
+    );
+}
+
+const MemoizedMyList = React.memo(MyList);
+
 
 export default ChaptersTab;
