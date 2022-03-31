2fd40226d885d1b3b43c9cf4bbd29bea13d47ea8
goblin <jackmord445@gmail.com>
2022-03-31 14:47:26 +0800

    don't use blob

--- public/DownloaderWorkerRequest.js
+++ public/DownloaderWorkerRequest.js
@@ -1,14 +1,13 @@
 function request(url, progress, success, error){
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
-    xhr.responseType = 'blob';
     xhr.onprogress = function(e){
         if (e.lengthComputable){
             progress(e.loaded, e.total);
         }
     };
     xhr.onload = function(){
-        if (this.status == 200){
+        if (this.status === 200){
             success(this.response);
         } else {
             error(this.status);
