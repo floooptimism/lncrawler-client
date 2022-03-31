cc4a735a1b799d010fd7c2fb5a995a8e4b5926b6
goblin <jackmord445@gmail.com>
2022-03-31 14:48:57 +0800

    updated

--- src/hooks/callbacks/downloaderSuccessCallback.js
+++ src/hooks/callbacks/downloaderSuccessCallback.js
@@ -4,9 +4,8 @@ import Librarian from "../../functionalcomponents/Librarian/Librarian";
 // I want to store those downloaded results into DB
 async function successCallback(param, callback){
     // 
-    console.log("success callback ->", param);
-    let content = await param.result.text();
-    await Librarian.storeChapterContent(param.task.novel, param.task.chapterIndex, content);
+    console.log("SUCCESSCALLBACK")
+    await Librarian.storeChapterContent(param.task.novel, param.task.chapterIndex, param.result);
 }
 
 export default successCallback;
