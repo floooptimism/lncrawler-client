e91f5ee6abc963077b77d8b50cdab7d0150dc890
goblin <jackmord445@gmail.com>
2022-03-31 14:50:00 +0800

    add a few attributes

--- src/pages/NovelInfoLibrary/Tabs/InformationTab.jsx
+++ src/pages/NovelInfoLibrary/Tabs/InformationTab.jsx
@@ -1,14 +1,14 @@
 import styles from "./Tabs.module.css";
 
-function InformationTab(props) {
-
+function InformationTab({novelInfo}) {
+    
     return (
         <>
             {/* chapters and reading progress */}
             <div className="flex justify-between mt-5">
                 <div className={styles.Attribute}>
                     <h1>Chapters</h1>
-                    <p>800</p>
+                    <p>{novelInfo && novelInfo.numberOfChapters}</p>
                 </div>
                 <div className={styles.Attribute}>
                     <h1>Reading progress</h1>
@@ -20,9 +20,7 @@ function InformationTab(props) {
             <div className={styles.Attribute + " mt-6"}>
                 <h1>Description</h1>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Pellentesque euismod, urna eu tincidunt consectetur, nisi
-                    nisl aliquam nunc, eget consectetur nisl nunc eget nisl.
+                    {novelInfo && novelInfo.description}
                 </p>
             </div>
         </>
